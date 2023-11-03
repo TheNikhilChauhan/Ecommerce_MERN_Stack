c4dd4c8955213e58e981004c6c07eca798cdb3cf
Nikhil <ace.nikchauhan@gmail.com>
2023-11-04 02:34:59 +0530

    added login and signup frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
+import React from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LoginPage, SignupPage } from "./routes/Routes";
 
 function App() {
   return (
     <>
-      <h1 className=" text-blue-500">Ecommerce</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/sign-up" element={<SignupPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
